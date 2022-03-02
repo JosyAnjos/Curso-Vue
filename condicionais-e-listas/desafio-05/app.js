53759a17bdcfc9766ff8c74dbc2164ca2bb33b17
JosyAnjos <josyanjos.souza@gmail.com>
2022-03-02 19:32:06 -0300

    Desafio 05

---
+++ condicionais-e-listas/desafio-05/app.js
@@ -0,0 +1,17 @@
+new Vue({
+  el: '#desafio',
+  data: {
+    expressao: true,
+    nomes: ['Ana', 'Rafa', 'Gui'],
+    livro: {
+      titulo: 'O Senho dos Anéis',
+      autor: 'J.R.R Tolkiens',
+      volume: '3'
+    },
+    aluno: {
+      id: 10,
+      nome: 'Maria',
+      notas: [7.67, 8.76, 6.54, 9.21]
+    }
+  }
+})
